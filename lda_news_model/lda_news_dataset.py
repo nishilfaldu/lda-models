8004501548ae0e618db702f1a160441895ec52ba
Zenith Zack <faldund@outlook.com>
2022-01-08 16:07:07 +0530

    library delete commit

--- lda_news_model/lda_news_dataset.py
+++ lda_news_model/lda_news_dataset.py
@@ -5,7 +5,6 @@ from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
-import numpy as np
 import nltk
 from gensim import corpora, models
 from gensim.models.phrases import Phraser, Phrases
