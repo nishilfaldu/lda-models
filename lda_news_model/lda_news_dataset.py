1e7c2fbd1ee768a4f1a8fa2fd9f6d54272f69349
Zenith Zack <faldund@outlook.com>
2022-01-08 14:55:42 +0530

    added imports

---
+++ lda_news_model/lda_news_dataset.py
@@ -0,0 +1,10 @@
+# Necessary library imports
+import pandas as pd
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+import numpy as np
+import nltk
+from gensim import corpora, models
+
